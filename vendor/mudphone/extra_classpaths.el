5739d2dd1937cbf46ae1413fae12c15572d6746c
Kyle Oba <koba@mudphone.com>
2009-10-05 16:53:12 -0700

    updated notes on gnus and added hbase-runner to classpath for repl

--- vendor/mudphone/extra_classpaths.el
+++ vendor/mudphone/extra_classpaths.el
@@ -1,3 +1,4 @@
 (add-to-list 'extra-classpaths (concat *RUNA-ROOT* "/ikouclojure/src/"))
 (add-to-list 'extra-classpaths (concat *RUNA-ROOT* "/norvig_ai/src/"))
+(add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/hbase-runner/src/"))
 (add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/mudphone/"))
