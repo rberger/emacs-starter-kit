859eaada2b4dc3ebfff6e3369f450bc9b899c367
Kyle Oba <koba@mudphone.com>
2009-10-08 22:42:27 -0700

    updated with clojure contrib classpath for repl

--- vendor/mudphone/extra_classpaths.el
+++ vendor/mudphone/extra_classpaths.el
@@ -2,3 +2,4 @@
 (add-to-list 'extra-classpaths (concat *RUNA-ROOT* "/norvig_ai/src/"))
 (add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/hbase-runner/src/"))
 (add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/mudphone/"))
+(add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/clojure-contrib/src/"))
