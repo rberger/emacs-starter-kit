29e1ed280777208f1bcdef32dfadadb536e5d14f
Kyle Oba <koba@mudphone.com>
2009-12-08 09:09:44 -0800

    Add qlp and little-clojurian to classpath.

--- vendor/mudphone/extra_classpaths.el
+++ vendor/mudphone/extra_classpaths.el
@@ -1,5 +1,7 @@
 (add-to-list 'extra-classpaths (concat *RUNA-ROOT* "/ikouclojure/src/"))
 (add-to-list 'extra-classpaths (concat *RUNA-ROOT* "/norvig_ai/src/"))
 (add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/hbase-runner/src/"))
+(add-to-list 'extra-classpaths (concat *WORK-ROOT* "/qlp/src/"))
+(add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/little-clojurian/src/"))
 (add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/mudphone/"))
 (add-to-list 'extra-classpaths (concat *WORK-ROOT* "/clojure/clojure-contrib/src/"))
